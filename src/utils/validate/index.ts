d5766a015a07ba0956d4b2ac1abb96b2c57456ea
Фенов Кирилл <fenov@vbankcenter.ru>
2025-03-11 12:29:29 +0300

    removed unused vuelidate

--- src/utils/validate/index.ts
+++
@@ -1,7 +0,0 @@
-import type { BaseValidation } from '@vuelidate/core'
-
-export const getErrorMessage = (field: BaseValidation) => {
-  const message = field.$errors?.[0]?.$message
-
-  return (message && field.$dirty) ? { message } : null
-}
